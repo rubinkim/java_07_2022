3e820a000e370456c9e7b965e5312a546d5ff30b
rubinkim <kimyongha2011@gmail.com>
2022-07-31 15:44:56 +0900

    증감식이 i+=2인 경우의 for loop

---
+++ src/edu/Page_121.java
@@ -0,0 +1,12 @@
+package edu;
+
+public class Page_121 {
+
+	public static void main(String[] args) {
+        // 제어변수,반복조건식,증감식,반복실행문
+		for(int i=2; i<=10; i+=2) {
+			System.out.println(i);
+		}
+	}
+
+}
