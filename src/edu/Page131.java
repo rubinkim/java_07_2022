d46582d1799591bfedf242d51e1233488b964439
rubinkim <kimyongha2011@gmail.com>
2022-08-01 19:49:10 +0900

    세가지 반복문 비교

---
+++ src/edu/Page131.java
@@ -0,0 +1,26 @@
+package edu;
+
+public class Page131 {
+
+	public static void main(String[] args) {
+		// for ¹®
+		for(int i=10; i>10; i++) {
+			System.out.println("for OK");
+		}
+		
+		// while ¹®
+		int j = 10;
+		while(j<10) {
+			System.out.println("while OK");
+			j++;
+		}
+		
+		// do while ¹®
+		int k = 10;
+		do {
+			System.out.println("do while OK");
+			k++;
+		}while(k<10);
+	}
+
+}
