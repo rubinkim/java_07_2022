10a039678a63404b542b7cac43883c16e66b615a
rubinkim <kimyongha2011@gmail.com>
2022-08-02 15:15:44 +0900

    Calculate the total number of elements in diagonal

--- src/edu/Page_164.java
+++ src/edu/Page_164.java
@@ -18,6 +18,17 @@ public class Page_164 {
 			}
 			System.out.println("\n");
 		}
+		
+//arr어래이에서 diagonal에 위치한 값들의 합을 구하시오.
+		int sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				if(i == j) {
+					sum += arr[i][j];
+				}
+			}
+		}
+		System.out.println("합계 : " + sum);
 	}
 
 }
