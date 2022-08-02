de7ab5a4b3a818d175b3f9ca9575e13806c7a155
rubinkim <kimyongha2011@gmail.com>
2022-08-02 11:32:26 +0900

    Array 선언과 초기화, 기본개념

---
+++ src/edu/Page_148.java
@@ -0,0 +1,15 @@
+package edu;
+
+import java.util.Arrays;
+
+public class Page_148 {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {10,20,30,40,50};		
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println(arr);
+	}
+}
