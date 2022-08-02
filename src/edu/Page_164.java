da67fe0c0497083fcdb00aa1ccc87f8d30a51fcf
rubinkim <kimyongha2011@gmail.com>
2022-08-02 15:24:41 +0900

    왼쪽 아래 삼각형 합계 구하기

--- src/edu/Page_164.java
+++ src/edu/Page_164.java
@@ -28,7 +28,18 @@ public class Page_164 {
 				}
 			}
 		}
-		System.out.println("합계 : " + sum);
+		System.out.println("대각선 합계 : " + sum);
+		
+//왼쪽 삼각형 순회
+		int left_lower_sum = 0;
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+				if(i >= j) {
+					left_lower_sum += arr[i][j];
+				}
+			}
+		}
+		System.out.println("왼쪽 아래 삼각형 합계 : " + left_lower_sum);
 	}
 
 }
