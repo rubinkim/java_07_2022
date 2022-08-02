482ed9e4f3d2830e4d1eba081a325d46ce643db1
rubinkim <kimyongha2011@gmail.com>
2022-08-02 11:52:16 +0900

    arr = new int[] {1,2,3}을 이용해서 배열을 수정해봤다.

---
+++ src/edu/Page_151.java
@@ -0,0 +1,19 @@
+package edu;
+
+public class Page_151 {
+
+	public static void main(String[] args) {
+		int[] arr = {10,20,30,40,50};
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+		System.out.println();
+			
+//		arr의 길이를 5에서 3으로 줄여보자.
+		arr = new int[] {60,70,80};
+		for(int i=0; i<arr.length; i++) {
+			System.out.println(arr[i]);
+		}
+	}
+
+}
