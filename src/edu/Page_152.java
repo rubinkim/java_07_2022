c4fa6409a343a16aea2a46b975c864ac57885c67
rubinkim <kimyongha2011@gmail.com>
2022-08-02 12:05:59 +0900

    확장 for loop 이용하기

---
+++ src/edu/Page_152.java
@@ -0,0 +1,18 @@
+package edu;
+
+public class Page_152 {
+
+	public static void main(String[] args) {
+//		확장 for문 이용하기
+		int arr[] = {10,20,30,40,50};
+		for(int num:arr) {
+			System.out.println(num);
+		}
+		System.out.println();
+		
+		String[] names = {"rubin","jiyoung","kiwon","julie","sseri"};
+		for(String name:names) {
+			System.out.println(name);
+		}
+	}
+}
