b50d9dfe6951307b35e283a4724b0b5f552ec76e
rubinkim <kimyongha2011@gmail.com>
2022-09-14 17:30:45 +0900

    switch~case의 데이터 타입

--- src/edu/Page_115.java
+++ src/edu/Page_115.java
@@ -27,6 +27,18 @@ public class Page_115 {
 			break;
 		default: System.out.println(nation + "은(/는) 아시아/유럽/아메리카에 속해 있지 않습니다.");
 		}
+		
+		System.out.println("위의 switch~case문을 if~else if문으로 변경해 봅시다.");
+		System.out.println(nation);
+		
+		if((nation=="KOREA") || (nation=="JAPAN") || (nation=="CHINA")) {
+			System.out.println(nation + "은(/는) 아시아 국가입니다.");
+		}
+		else if((nation=="GREAT BRITAIN") || (nation=="GERMANY") || (nation=="FRANCE")) {
+			System.out.println(nation + "은(/는) 유럽 국가입니다.");
+		}
+		else if((nation=="USA") || (nation=="CANADA") || (nation=="MEXICO")) {
+			System.out.println(nation + "은(/는) 아메리카입니다.");
+		}
 	}
-
 }
