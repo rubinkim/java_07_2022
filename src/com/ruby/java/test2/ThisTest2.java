c22ca043c0ac2116bb04e042d34c1f1d6daa9a3a
rubinkim <kimyongha2011@gmail.com>
2022-10-10 18:46:46 +0900

    toString() & this()

--- src/com/ruby/java/test2/ThisTest2.java
+++ src/com/ruby/java/test2/ThisTest2.java
@@ -18,14 +18,18 @@ public class ThisTest2 {
 		this.age = age;
 	}
 	
+	public String toString() {
+		return name + " : " + age;
+	}
+	
 	public static void main(String[] args) {
 		ThisTest2 tt1 = new ThisTest2();
 		ThisTest2 tt2 = new ThisTest2("천지영");
 		ThisTest2 tt3 = new ThisTest2("김기원", 47);
 		
-		System.out.println("tt1의 name : " + tt1.name + "   tt1의 age : " + tt1.age);
-		System.out.println("tt2의 name : " + tt2.name + "   tt2의 age : " + tt2.age);
-		System.out.println("tt3의 name : " + tt3.name + "   tt3의 age : " + tt3.age);
+		System.out.println(tt1.toString());
+		System.out.println(tt2.toString());
+		System.out.println(tt3.toString());
 	}
 
 }
