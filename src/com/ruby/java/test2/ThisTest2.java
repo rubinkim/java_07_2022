1ff14bc6b4f24bdb628bfa24709982b3a25bb929
rubinkim <kimyongha2011@gmail.com>
2022-10-10 15:41:27 +0900

    Use this() to improve the maintenability of the code

---
+++ src/com/ruby/java/test2/ThisTest2.java
@@ -0,0 +1,30 @@
+package com.ruby.java.test2;
+
+public class ThisTest2 {
+	
+	private String name;
+	private int age;
+	
+	public ThisTest2() {
+		this("김한용");		
+	}
+	
+	public ThisTest2(String name) {
+		this(name, 64);
+	}
+	
+	public ThisTest2(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+	
+	public static void main(String[] args) {
+		ThisTest2 tt1 = new ThisTest2();
+		ThisTest2 tt2 = new ThisTest2("천지영");
+		ThisTest2 tt3 = new ThisTest2("김기원", 47);
+		
+		System.out.println("tt1의 name : " + tt1.name);
+		System.out.println("tt2의 name : " + tt2.name);
+	}
+
+}
