f6b846393bb25c8d1bbab95176fc454ff6d95e5a
rubinkim <kimyongha2011@gmail.com>
2022-10-10 15:46:51 +0900

    Object Oriented Programming

--- src/com/ruby/java/test2/ThisTest2.java
+++ src/com/ruby/java/test2/ThisTest2.java
@@ -3,7 +3,7 @@ package com.ruby.java.test2;
 public class ThisTest2 {
 	
 	private String name;
-	private int age;
+	private int age = 0;
 	
 	public ThisTest2() {
 		this("김한용");		
@@ -25,6 +25,7 @@ public class ThisTest2 {
 		
 		System.out.println("tt1의 name : " + tt1.name + "   tt1의 age : " + tt1.age);
 		System.out.println("tt2의 name : " + tt2.name + "   tt2의 age : " + tt2.age);
+		System.out.println("tt3의 name : " + tt3.name + "   tt3의 age : " + tt3.age);
 	}
 
 }
