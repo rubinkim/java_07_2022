b8a191cee2a033574b5ea96972e7cbddab78e42b
rubinkim <kimyongha2011@gmail.com>
2022-10-12 21:27:54 +0900

    practice data type conversion

---
+++ src/com/ruby/java/test2/PracticeBinary.java
@@ -0,0 +1,14 @@
+package com.ruby.java.test2;
+
+public class PracticeBinary {
+
+	public static void main(String[] args) {
+		byte a = 25;
+		byte b = 19;
+		
+		byte c = a+b;
+		
+		System.out.println(c);
+	}
+
+}
