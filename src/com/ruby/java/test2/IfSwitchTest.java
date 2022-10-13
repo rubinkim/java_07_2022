6f1ea75aaf188ed287d8d6012fadacc87c5da3d3
rubinkim <kimyongha2011@gmail.com>
2022-10-13 11:10:17 +0900

    SWITCH문이 실행이 안된다.

--- src/com/ruby/java/test2/IfSwitchTest.java
+++ src/com/ruby/java/test2/IfSwitchTest.java
@@ -6,21 +6,37 @@ public class IfSwitchTest {
 		
 		// if문으로 실행해보기
 		int score1 = 83;
-		char grade;
+		char grade1;
 		
 		if(score1 >= 90) {
-			grade = 'A';
+			grade1 = 'A';
 		}
 		else if(score1 >= 80) {
-			grade = 'B';
+			grade1 = 'B';
 		}
 		else if(score1 >= 70) {
-			grade = 'C';
+			grade1 = 'C';
 		}
 		else {
-			grade = 'F';
+			grade1 = 'F';
 		}
-		System.out.println("IF문으로 실행한 grade는 " + grade + "이다.");
+		System.out.println("IF문으로 실행한 grade는 " + grade1 + "이다.");
+		
+		
+		// switch문을 실행해보기
+		int score2 = 83;
+		char grade2;
+		
+		switch(score2) {
+		case 90: grade2 = 'A';
+		case 83: grade2 = 'B';
+		case 75: grade2 = 'C';
+		default: grade2 = 'F';
+		}
+		
+		System.out.println("SWITCH문으로 실행한 grade는 " + grade2 + "이다.");
+		System.out.println(score2 + "  " + grade2);
+		
+		
 	}
-
 }
