82ec6d1eadc7f398c9bb326a85d5c7333456b500
rubinkim <kimyongha2011@gmail.com>
2022-10-14 17:16:29 +0900

    Scanner sc = new Scanner(System.in);

---
+++ src/com/ruby/java/test2/SwitchTest.java
@@ -0,0 +1,20 @@
+package com.ruby.java.test2;
+
+import java.util.Scanner;
+
+public class SwitchTest {
+
+	//연산자값 가져오기
+	int a = 12;  int b = 2;
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println(" +, -, *, / 중 하나의 연산자를 입력하세요 : ");
+		op = sc.next();
+		
+		
+		
+	}
+
+}
