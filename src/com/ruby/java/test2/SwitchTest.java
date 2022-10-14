2e398a42f84b2cb8b2c99de4be90342817aa204b
rubinkim <kimyongha2011@gmail.com>
2022-10-14 17:37:09 +0900

    More on Scanner & switch ~ case

--- src/com/ruby/java/test2/SwitchTest.java
+++ src/com/ruby/java/test2/SwitchTest.java
@@ -4,19 +4,23 @@ import java.util.Scanner;
 
 public class SwitchTest {
 
-	//연산자값 가져오기
-	int a = 12;  int b = 2;
-	
 	public static void main(String[] args) {
+		
+		//연산자값 가져오기
+		int a = 12;  int b = 2;
+		int answer = 0;
 		
 		Scanner sc = new Scanner(System.in);
 		System.out.println(" +, -, *, / 중 하나의 연산자를 입력하세요 : ");
 		String op = sc.next();
 		
 		switch(op) {
-		case
+		case "+": answer = a + b; break;
+		case "-": answer = a - b; break;
+		case "*": answer = a * b; break;
+		case "/": answer = a / b; break;
 		}
 		
+		System.out.println("선택된 연산자는 " + op + "로써 계산된 값은 " + answer + "이다.");
 	}
-
 }
