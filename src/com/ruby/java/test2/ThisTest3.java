c2aae9f75ff92525beecd172b23e51dfb68d1ab6
rubinkim <kimyongha2011@gmail.com>
2022-10-17 16:53:03 +0900

    점수들 합계,평균,최고,최저 계산하기

--- src/com/ruby/java/test2/ThisTest3.java
+++ src/com/ruby/java/test2/ThisTest3.java
@@ -18,6 +18,27 @@ public class ThisTest3 {
 		for(char c : alphabet) {
 			System.out.print(c);
 		}
+		
+		System.out.println();
+		
+		// 배열에 저장된 성적들의 합계,평균,최고,최저 점수 구하기
+		int[] score = {93,100,72,91,87};
+		int sum = 0;
+		int max = 0;
+		int min = 999;
+		float avg = 0.0f;
+		
+		for(int i=0; i<score.length; i++) {
+			sum += score[i];
+			if(max < score[i]) max = score[i];
+			if(min > score[i]) min = score[i]; 
+		}
+		
+		avg = sum / score.length;
+		
+		System.out.println("성적 합계는 : " + sum);
+		System.out.println("성적 평균은 : " + avg);
+		System.out.println("최고 성적은 : " + max);
+		System.out.println("최저 성적은 : " + min);
 	}
-
 }
