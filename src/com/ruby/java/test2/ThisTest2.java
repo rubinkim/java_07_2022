9c0823457a850dfe453b7b042125428d6bbaa8f0
rubinkim <kimyongha2011@gmail.com>
2022-10-18 18:28:26 +0900

    More on command line parameter

--- src/com/ruby/java/test2/ThisTest2.java
+++ src/com/ruby/java/test2/ThisTest2.java
@@ -30,6 +30,12 @@ public class ThisTest2 {
 		System.out.println(tt1.toString());
 		System.out.println(tt2.toString());
 		System.out.println(tt3.toString());
+		
+		System.out.println('\n');
+		
+		for(int i=0; i<args.length; i++) {
+			System.out.println(args[i]);
+		}
 	}
 
 }
