5cdbb4a7e3c74404fe7de2243a1bf524fc81e3de
rubinkim <kimyongha2011@gmail.com>
2022-10-23 19:28:46 +0900

    Modified Test_102322_01.java

--- new_java_test/Test_102322_01.java
+++ new_java_test/Test_102322_01.java
@@ -1,8 +1,8 @@
-package bigData.infoProcess.rubin.2022_10;
+package bigData.infoProcess.rubin.test202210;
 
 class Test_102322_01{
     public static void main(String[] args){
         System.out.println("I acquired BigData Expert License.");
-        System.out.println("I acquired InfoProcessing Expert License.")
+        System.out.println("I acquired InfoProcessing Expert License.");
     }
 }
