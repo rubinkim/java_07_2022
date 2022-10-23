7ed2ea389a03be86d0b669dabb943515eb6fbb45
rubinkim <kimyongha2011@gmail.com>
2022-10-23 19:20:56 +0900

    Modified Test_102223_01.java

--- new_java_test/Test_102322_01.java
+++ new_java_test/Test_102322_01.java
@@ -3,8 +3,8 @@ package bigData.infoProcess.rubin.2022_10;
 
 class Test_102322_01{
     public static void main(String[] args){
-        System.out.println("나는 빅데이터 분석기사 자격증을 취득했다.");
-        System.out.println("나는 정보처리기사 자격증을 취득했다.")
+        System.out.println("I acquired BigData Expert License.");
+        System.out.println("I acquired InfoProcessing Expert License.")
     }
 }
 ```
