ebd7f78484024751cd44bc4cb8112ed0fd040c76
rubinkim <kimyongha2011@gmail.com>
2022-10-26 18:09:05 +0900

    복합선언 실행 완료

--- src/com/ruby/java/test2/ChangeableLengthArgumentTest.java
+++ src/com/ruby/java/test2/ChangeableLengthArgumentTest.java
@@ -13,12 +13,25 @@ public class ChangeableLengthArgumentTest {
 	}
 	
 	// 가변길이 인자를 전달받는 매개변수와 더불어서 일반인자도 전달받는 복합선언인 경우
-	static void 
+	static void test2(String name, int ... v) {
+		System.out.print("첫번째 전달받은 인자는 " + name + "이고.");
+		System.out.print("두번째 전달받은 인자는 ");
+		for(int x : v) {
+			System.out.print(x + " ");
+		}
+		System.out.print("이다.");
+		System.out.println();
+	}
 	
 	public static void main(String[] args) {
 		test1(1);
 		test1(1,2);
 		test1(1,2,3);
+		
+		System.out.println();
+		
+		test2("김한용", 1);
+		test2("천지영", 1,2);
+		test2("김기원", 1,2,3);
 	}
-
 }
