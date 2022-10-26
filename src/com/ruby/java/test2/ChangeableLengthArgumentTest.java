83d742a9f96ef5c46ba009537fce5badf8a4779a
rubinkim <kimyongha2011@gmail.com>
2022-10-26 17:52:46 +0900

    가변길이 인자를 전달받는 매개변수만 있는 경우

--- src/com/ruby/java/test2/ChangeableLengthArgumentTest.java
+++ src/com/ruby/java/test2/ChangeableLengthArgumentTest.java
@@ -1,9 +1,21 @@
 package com.ruby.java.test2;
 
 public class ChangeableLengthArgumentTest {
-
+	// 가변길이 인자를 전달받는 매개변수만 있는 메서드 선언
+	static void test1(int ... v) {
+		System.out.print("매개변수의 개수는 " + v.length + "개이고 ");
+		System.out.print("전달받은 인자는 ");
+		for(int x : v) {
+			System.out.print(x + " ");
+		}
+		System.out.print("이다.");
+		System.out.println();
+	}
+	
 	public static void main(String[] args) {
-
+		test1(1);
+		test1(1,2);
+		test1(1,2,3);
 	}
 
 }
