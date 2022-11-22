67e38be22e68a05ebe4f2c6c6c568260efeb7cea
rubinkim <kimyongha2011@gmail.com>
2022-11-22 20:28:29 +0900

    Compiling StudentNew.java

--- src/com/ruby/java/ch07/inheritance/StudentNew.java
+++ src/com/ruby/java/ch07/inheritance/StudentNew.java
@@ -1,5 +1,12 @@
 package com.ruby.java.ch07.inheritance;
 
 public class StudentNew {
-
+	private String major;
+	
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
 }
