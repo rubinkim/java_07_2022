f881f15cd040f7539b721bb0fa6159ed1d57fd9d
rubinkim <kimyongha2011@gmail.com>
2022-11-22 20:23:42 +0900

    Compiling ProfessorNew.java

--- src/com/ruby/java/ch07/inheritance/ProfessorNew.java
+++ src/com/ruby/java/ch07/inheritance/ProfessorNew.java
@@ -1,5 +1,12 @@
 package com.ruby.java.ch07.inheritance;
 
 public class ProfessorNew {
-
+	private String subject;
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
 }
