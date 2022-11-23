22e1224fcc69127801080f96a7a9ceabd10e2bec
rubinkim <kimyongha2011@gmail.com>
2022-11-23 20:53:05 +0900

    Creating Professor.java

--- src/com/ruby/java/ch07/inheritance_new/LMSTest.java
+++ src/com/ruby/java/ch07/inheritance_new/LMSTest.java
@@ -4,11 +4,17 @@ public class LMSTest {
 
 	public static void main(String[] args) {
 		Employee e = new Employee();
+		Professor p = new Professor();
 		
 		e.setName("김한용");
 		e.setAge(64);
 		e.setDept("빅데이터");
 		
+		p.setName("천지영");
+		p.setAge(45);
+		p.setSubject("공예");
+		
 		System.out.println(e.toString());
+		System.out.println(p.toString());
 	}
 }
