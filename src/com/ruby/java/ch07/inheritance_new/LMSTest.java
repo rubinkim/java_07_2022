6b36f4ace7c0f73a5ce4ac7e2bd60c6700a4aeff
rubinkim <kimyongha2011@gmail.com>
2022-11-23 20:35:31 +0900

    Creating LMSTest.java

---
+++ src/com/ruby/java/ch07/inheritance_new/LMSTest.java
@@ -0,0 +1,14 @@
+package com.ruby.java.ch07.inheritance_new;
+
+public class LMSTest {
+
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		
+		e.setName("±Ë«—øÎ");
+		e.setAge(64);
+		e.setDept("∫Úµ•¿Ã≈Õ");
+		
+		System.out.println(e.toString());
+	}
+}
