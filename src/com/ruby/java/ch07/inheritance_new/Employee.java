f7a0d250b947e0d580ce714b8eec5284f37e374d
rubinkim <kimyongha2011@gmail.com>
2022-11-24 19:21:43 +0900

    상속, 기본생성자, 추가생성자...

--- src/com/ruby/java/ch07/inheritance_new/Employee.java
+++ src/com/ruby/java/ch07/inheritance_new/Employee.java
@@ -3,6 +3,16 @@ package com.ruby.java.ch07.inheritance_new;
 public class Employee extends Person {
 	private String dept;
 	
+	public Employee() {
+		System.out.println("Employee 클래스의 기본 생성자");
+	}
+	
+	public Employee(String name, int age, String dept) {
+		super.setName(name);
+		super.setAge(age);
+		this.dept = dept;
+	}
+	
 	public String getDept() {
 		return dept;
 	}
@@ -11,6 +21,7 @@ public class Employee extends Person {
 	}
 	
 	public String toString() {
-		return getName() + " : " + getAge() + " ==> " + dept;
+		System.out.println("e 인스턴스의 필드 값을 살펴보자.");
+		return super.getName() + " : " + super.getAge() + " ==> " + this.dept;
 	}
 }
--- src/com/ruby/java/ch07/inheritance_new/LMSTest2.java
+++ src/com/ruby/java/ch07/inheritance_new/LMSTest2.java
@@ -3,10 +3,7 @@ package com.ruby.java.ch07.inheritance_new;
 public class LMSTest2 {
 
 	public static void main(String[] args) {
-		Employee e = new Employee();
-		e.setName("¿Ã¿Ø∫Û");
-		e.setAge(21);
-		e.setDept("ƒƒ«ª≈Õ ªÁ¿ÃæΩ∫");
+		Employee e = new Employee("±Ë¿Ø∫Û", 22, "computer science");
 		System.out.println(e.toString());
 		
 		Professor p = new Professor();
@@ -14,6 +11,12 @@ public class LMSTest2 {
 		p.setAge(38);
 		p.setSubject("Volley Ball");
 		System.out.println(p.toString());
+		
+		Student s = new Student();
+		s.setName("±Ë«œ∞Ê");
+		s.setAge(27);
+		s.setMajor("Setter");
+		System.out.println(s.toString());
 	}
 
 }
