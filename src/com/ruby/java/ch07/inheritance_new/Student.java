d067e0fd1d144d346e2c621e401da241c7e8c9c1
rubinkim <kimyongha2011@gmail.com>
2022-11-24 19:43:36 +0900

    Student클래스의 기본, 추가생성자 구현

--- src/com/ruby/java/ch07/inheritance_new/Student.java
+++ src/com/ruby/java/ch07/inheritance_new/Student.java
@@ -3,7 +3,18 @@ package com.ruby.java.ch07.inheritance_new;
 public class Student extends Person {
 	private String major;
 	
-	public String getMajor() {
+	public Student() {
+		System.out.println("Student클래스의 기본 생성자이다.");
+	}
+	
+	public Student(String name, int age, String major) {
+		super.setName(name);
+		super.setAge(age);
+		this.major = major;
+	}
+	
+	public String getMajor()
+	{
 		return major;
 	}
 	public void setMajor(String major) {
