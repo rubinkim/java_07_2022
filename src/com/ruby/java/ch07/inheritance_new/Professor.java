ab8adc45f1f46c415faa920e01865110d1b3a533
rubinkim <kimyongha2011@gmail.com>
2022-11-24 19:29:18 +0900

    Professor 클래스의 기본생성자 구현

--- src/com/ruby/java/ch07/inheritance_new/Professor.java
+++ src/com/ruby/java/ch07/inheritance_new/Professor.java
@@ -3,6 +3,10 @@ package com.ruby.java.ch07.inheritance_new;
 public class Professor extends Person {
 	private String subject;
 	
+	public Professor() {
+		System.out.println("Professor 클래스의 defalut constructor");
+	}
+	
 	public String getSubject() {
 		return subject;
 	}
