fe508dd7aadf6c871c68ac75d49ab6662bce81bf
rubinkim <kimyongha2011@gmail.com>
2022-11-28 18:40:04 +0900

    Creating an instance of Salesman class

--- src/com/ruby/java/ch07/abstraction_new/HRSTest.java
+++ src/com/ruby/java/ch07/abstraction_new/HRSTest.java
@@ -3,7 +3,10 @@ package com.ruby.java.ch07.abstraction_new;
 public class HRSTest {
 
 	public static void main(String[] args) {
-
+		
+		Salesman s = new Salesman();
+		s.calcSalary();
+		
 	}
 
 }
