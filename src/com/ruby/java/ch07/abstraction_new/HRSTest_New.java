c1df07360e2e7814731638945ad68cc326989866
rubinkim <kimyongha2011@gmail.com>
2022-11-29 19:03:05 +0900

    Beginnig HRSTest_New.java

---
+++ src/com/ruby/java/ch07/abstraction_new/HRSTest_New.java
@@ -0,0 +1,17 @@
+package com.ruby.java.ch07.abstraction_new;
+
+abstract class Employee{
+	String name;
+	int salary;
+	
+	public abstract void calcSalary();
+	public abstract void calcBonus();
+}
+
+public class HRSTest_New {
+
+	public static void main(String[] args) {
+
+	}
+
+}
