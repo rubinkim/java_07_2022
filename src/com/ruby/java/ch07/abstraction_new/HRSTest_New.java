627aed3af49ab34f940dc1fe2684c8d30798d3ba
rubinkim <kimyongha2011@gmail.com>
2022-11-29 19:10:36 +0900

    You must use public modifier to declare a class only once!!!!

--- src/com/ruby/java/ch07/abstraction_new/HRSTest_New.java
+++ src/com/ruby/java/ch07/abstraction_new/HRSTest_New.java
@@ -1,6 +1,6 @@
 package com.ruby.java.ch07.abstraction_new;
 
-abstract class Employee{
+abstract class Employee1{
 	String name;
 	int salary;
 	
@@ -8,10 +8,22 @@ abstract class Employee{
 	public abstract void calcBonus();
 }
 
+class Salesman1 extends Employee1{
+	public void calcSalary() {
+		System.out.println("Salesman 급여 = 기본급 + 기본 수당");
+	}
+	public void calcBonus() {
+		System.out.println("Salesman 보너스 = 기본급 * 12 * 4");
+	}
+}
+
 public class HRSTest_New {
 
 	public static void main(String[] args) {
-
+		
+		Salesman1 s = new Salesman1();
+		
+		s.calcSalary();
 	}
 
 }
