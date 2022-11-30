27009f9c715dc11f9aa299cdeca70b6628f3c3d4
rubinkim <kimyongha2011@gmail.com>
2022-11-30 19:50:26 +0900

    Creating an interface method

--- src/com/ruby/java/ch07/abstraction_new/Messenger.java
+++ src/com/ruby/java/ch07/abstraction_new/Messenger.java
@@ -5,6 +5,9 @@ public interface Messenger {
 	public static final int MIN_SIZE = 0;
 	public static final int MAX_SIZE = 104857600;
 	
+	public abstract String getMessage();
+	public abstract void setMessage(String msg);
+	
 	
 	
 }
