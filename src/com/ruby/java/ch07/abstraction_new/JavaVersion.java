53837ac9529a61533d7221e65ec9cf8d701a7c40
rubinkim <kimyongha2011@gmail.com>
2022-12-01 21:20:52 +0900

    Creating JavaVersion class

---
+++ src/com/ruby/java/ch07/abstraction_new/JavaVersion.java
@@ -0,0 +1,9 @@
+package com.ruby.java.ch07.abstraction_new;
+
+public class JavaVersion {
+
+	public static void main(String[] args) {
+		System.out.println(System.getProperty("java.runtime.version"));
+	}
+
+}
--- src/com/ruby/java/ch07/abstraction_new/Messenger.java
+++ src/com/ruby/java/ch07/abstraction_new/Messenger.java
@@ -2,5 +2,25 @@ package com.ruby.java.ch07.abstraction_new;
 
 public interface Messenger {
 
+	public static final int MIN_SIZE = 1;
+	public static final int MAX_SIZE = 104857600;
 	
-}
+	public abstract String getMessage();
+	public abstract void setMessage(String msg);
+
+	public default void setLogin(boolean login) {
+
+		if(login) {
+			System.out.println("로그인 처리합니다.");
+		} else {
+			System.out.println("로그아웃 처리합니다.");
+		}
+	}
+	
+	public static void getConnection() {
+		System.out.println("network에 연결합니다.");
+	}
+	
+
+	}
+
