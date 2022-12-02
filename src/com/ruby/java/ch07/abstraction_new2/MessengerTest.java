a306b702b78495ce85677f1a041ede4811fd6ca3
rubinkim <kimyongha2011@gmail.com>
2022-12-02 19:43:47 +0900

    Creating a class called MessengerTest.java

---
+++ src/com/ruby/java/ch07/abstraction_new2/MessengerTest.java
@@ -0,0 +1,14 @@
+package com.ruby.java.ch07.abstraction_new2;
+
+public class MessengerTest {
+
+	public static void main(String[] args) {
+		IPhoneMessenger iphone = new IPhoneMessenger();
+		
+		System.out.println("메신저 최소문자 크기 : " + Messenger.MIN_SIZE);
+		System.out.println("메신저 최대문자 크기 : " + Messenger.MAX_SIZE);
+		
+		System.out.println(iphone.getMessage());
+		iphone.clearMessage();
+	}
+}
