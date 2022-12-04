d01871a6c802f75d968424edaac3dd0ddbd5b6c3
rubinkim <kimyongha2011@gmail.com>
2022-12-04 21:00:46 +0900

    Creating Consultant class

--- src/com/ruby/java/ch08/polymorphism_new/HRSTest.java
+++ src/com/ruby/java/ch08/polymorphism_new/HRSTest.java
@@ -9,7 +9,7 @@ public class HRSTest {
 		public abstract void calcBonus();
 	}
 	
-	class Salesman extends Employee {
+	class Salesman extends Employee{
 		public void calcSalary() {
 			System.out.println("Salesman급여 = 기본급 + 판매수당");
 		}
@@ -17,9 +17,18 @@ public class HRSTest {
 			System.out.println("Salesman보너스 = 기본급 * 12 * 4");
 		}
 	}
+	
+	class Consultant extends Employee{
+		public void calcSalary() {
+			System.out.println("Consultant급여 = 기본급 + 컨설팅 특별수당");
+		}
+		public void calcBonus() {
+			System.out.println("Consultant보너스 = 기본급 * 12 *6");
+		}
+	}
 
 	public static void main(String[] args) {
-
+		
 	}
 
 }
