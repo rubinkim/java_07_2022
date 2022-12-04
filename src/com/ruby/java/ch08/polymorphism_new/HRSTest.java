bb768f00aff5134241c9f9ef2b88177144e32994
rubinkim <kimyongha2011@gmail.com>
2022-12-04 20:52:26 +0900

    Creating HRSTest class

---
+++ src/com/ruby/java/ch08/polymorphism_new/HRSTest.java
@@ -0,0 +1,16 @@
+package com.ruby.java.ch08.polymorphism_new;
+
+public class HRSTest {
+	
+	abstract class Employee{
+		String name;
+		int salary;
+		public abstract void calcSalary();
+		public abstract void calcBonus();
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
