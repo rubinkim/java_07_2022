dc1889866dddaff5ad8db949e980a6f2cc336ba9
rubinkim <kimyongha2011@gmail.com>
2022-12-04 21:21:16 +0900

    Restructuring HRSTest.java

--- src/com/ruby/java/ch08/polymorphism_new/HRSTest.java
+++ src/com/ruby/java/ch08/polymorphism_new/HRSTest.java
@@ -1,51 +1,52 @@
 package com.ruby.java.ch08.polymorphism_new;
 
-public class HRSTest {
+abstract class Employee{
+	String name;
+	int salary;
+	public abstract void calcSalary();
+	public abstract void calcBonus();
+}
 	
-	abstract class Employee{
-		String name;
-		int salary;
-		public abstract void calcSalary();
-		public abstract void calcBonus();
+class Salesman extends Employee{
+	public void calcSalary() {
+		System.out.println("Salesman급여 = 기본급 + 판매수당");
 	}
+	public void calcBonus() {
+		System.out.println("Salesman보너스 = 기본급 * 12 * 4");
+	}
+}
 	
-	class Salesman extends Employee{
-		public void calcSalary() {
-			System.out.println("Salesman급여 = 기본급 + 판매수당");
-		}
-		public void calcBonus() {
-			System.out.println("Salesman보너스 = 기본급 * 12 * 4");
-		}
+class Consultant extends Employee{
+	public void calcSalary() {
+		System.out.println("Consultant급여 = 기본급 + 컨설팅 특별수당");
 	}
+	public void calcBonus() {
+		System.out.println("Consultant보너스 = 기본급 * 12 *6");
+	}
+}
 	
-	class Consultant extends Employee{
-		public void calcSalary() {
-			System.out.println("Consultant급여 = 기본급 + 컨설팅 특별수당");
-		}
-		public void calcBonus() {
-			System.out.println("Consultant보너스 = 기본급 * 12 *6");
-		}
+abstract class Manager extends Employee{
+	public void calcSalary() {
+		System.out.println("Manager급여 = 기본급 + 팀성과수당");
 	}
+}
 	
-	abstract class Manager extends Employee{
-		public void calcSalary() {
-			System.out.println("Manager급여 = 기본급 + 팀성과수당");
-		}
+class Director extends Manager{
+	public void calcBonus() {
+		System.out.println("Director보너스 = 기본급 * 12 * 12");
 	}
+}
 	
-	class Director extends Manager{
-		public void calcBonus() {
-			System.out.println("Director보너스 = 기본급 * 12 * 12");
-		}
-	}
+public class HRSTest{
 	
 	public static void calcTax(Employee e) {
-		System.out.println(e + "의 소득세을 계산한다.");
+		System.out.println(e + "의 소득세를 계산한다.");
 	}
 	
-	public static void main(String[] args) {
-
-		
-	}
-
+	
 }
+	
+
+	
+
+
