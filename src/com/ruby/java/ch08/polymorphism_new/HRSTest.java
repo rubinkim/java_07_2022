ad229183192770b08f2da8ed2a1d63f683088ce8
rubinkim <kimyongha2011@gmail.com>
2022-12-04 20:55:34 +0900

    Creating Salesman class

--- src/com/ruby/java/ch08/polymorphism_new/HRSTest.java
+++ src/com/ruby/java/ch08/polymorphism_new/HRSTest.java
@@ -8,6 +8,15 @@ public class HRSTest {
 		public abstract void calcSalary();
 		public abstract void calcBonus();
 	}
+	
+	class Salesman{
+		public void calcSalary() {
+			System.out.println("Salesman급여 = 기본급 + 판매수당");
+		}
+		public void calcBonus() {
+			System.out.println("Salesman보너스 = 기본급 * 12 * 4");
+		}
+	}
 
 	public static void main(String[] args) {
 
