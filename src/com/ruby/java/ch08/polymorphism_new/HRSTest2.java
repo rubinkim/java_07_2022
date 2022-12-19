1793f2ff55cab03f6b86359fa56f2e35a785aaca
rubinkim <kimyongha2011@gmail.com>
2022-12-19 20:12:56 +0900

    abstract class Consultant extends Employee

--- src/com/ruby/java/ch08/polymorphism_new/HRSTest2.java
+++ src/com/ruby/java/ch08/polymorphism_new/HRSTest2.java
@@ -54,6 +54,26 @@ class Consultant extends Employee{
 	}
 }
 
+abstract class Manager extends Employee{
+	public void calcSalary() {
+		System.out.println("Manager급여 = 기본급 + 팀성과수당");
+	}
+}
+
+class Director extends Manager{
+	// constructor 생성
+	public Director() {
+	}
+	public Director(String name) {
+		this.name = name;
+	}
+	
+	// 추상메서드 구현하기
+	public void calcBonus() {
+		System.out.println("Director보너스 = 기본급 * 12 * 12");
+	}
+}
+
 public class HRSTest2 {
 
 	public static void main(String[] args) {
